e13151d1aab936ab8522d8dd5aed5cb3c216bf9d
Gabriel Porcellis <gabrielporcellis@gmail.com>
2020-12-22 14:30:31 -0300

    criada classe solicitações

---
+++ ProjetoInventti/ProjetoInventti/Solicitacoes.cs
@@ -0,0 +1,34 @@
+﻿using ProjetoInventti.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetoInventti
+{
+    public class Solicitacoes
+    {
+        public DateTime DataSolicitacao { get; set; }
+        public string Titulo { get; set; }
+        public Pessoa Nome{ get; set; }
+        public string Descricao { get; set; }
+
+        public Solicitacoes(DateTime dataSolicitacao, string titulo, Pessoa nome, string descricaozinha)
+        {
+            DataSolicitacao = dataSolicitacao;
+            Titulo = titulo;
+            Nome = nome;
+            Descricao = descricaozinha;
+        }
+
+        //Formatação do objeto
+        public override string ToString()
+        {
+            return "Data da solicitação : "
+                + DataSolicitacao + "\n"
+                + "Título: "
+                + Titulo + "\n"
+                + "Descrição: "
+                + Descricao
+        }
+    }
+}
