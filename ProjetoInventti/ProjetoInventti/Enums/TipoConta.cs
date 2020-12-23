3dcd96869b5e35e141248809d9f166598d8bc112
Gabriel Porcellis <gabrielporcellis@gmail.com>
2020-12-23 15:46:13 -0300

    Criada classe num TipoConta + adequações em várias classes

---
+++ ProjetoInventti/ProjetoInventti/Enums/TipoConta.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetoInventti.Enums
+{
+    public enum TipoConta
+    {
+        //Classe ainda não utilizada
+        Energia,
+        Agua,
+        Gas,
+        Jardineiro,
+        DespesasGerais,
+        Multa,
+        Condominio,
+        SalarioZelador,
+        SalarioSindico
+    }
+}
